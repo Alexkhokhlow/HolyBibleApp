3fb9aa2376afd70b781310a32d8ddb92fc68353f
root <root@root.com>
2022-04-24 19:03:24 +0300

    HB01 domain layer for books

--- app/src/main/java/by/brust/holybibleapp/core/BibleApp.kt
+++ app/src/main/java/by/brust/holybibleapp/core/BibleApp.kt
@@ -1,10 +1,16 @@
 package by.brust.holybibleapp.core
 
 import android.app.Application
+import by.brust.holybibleapp.data.BooksRepository
+import by.brust.holybibleapp.domain.BaseBooksDataToDomainMapper
+import by.brust.holybibleapp.domain.BookInteractor
 
-class BibleApp: Application() {
+class BibleApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        val booksRepository: BooksRepository = TODO("merge")
+        val bookInteractor = BookInteractor.Base(booksRepository, BaseBooksDataToDomainMapper())
     }
 }
--- app/src/main/java/by/brust/holybibleapp/data/BooksData.kt
+++ app/src/main/java/by/brust/holybibleapp/data/BooksData.kt
@@ -3,6 +3,6 @@ package by.brust.holybibleapp.data
 import by.brust.holybibleapp.core.Abstract
 import by.brust.holybibleapp.domain.BookDomain
 
-sealed class BookData: Abstract.Object<BookDomain, Abstract.Mapper.Empty>() {
+sealed class BooksData: Abstract.Object<BookDomain, BooksDataToDomainMapper>() {
     //todo fix
 }
